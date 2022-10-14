54e58524da91f06cbb01833041f13aaf31ce008b
Sameazzy <sammyeazzy@gmail.com>
2022-10-14 14:52:30 -0700

    positive, negative, or zero?

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
-//**
-/* main - Entry point
- *
- * Return: Always 1 (Success)
- */
-int main(){
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", 57)
-	return (1);
-}
 
+/**
+ * main - Entry point
+ * Description: main explained
+ *
+ * Return Always 0 (Succes)
+*/
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	return (0);
+}

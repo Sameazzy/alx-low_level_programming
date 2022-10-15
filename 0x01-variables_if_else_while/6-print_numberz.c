ded74fdfb4db27e9df0506f7b6bcfda1cabed67e
Sameazzy <sammyeazzy@gmail.com>
2022-10-14 21:31:56 -0700

    single numbers in base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * entry point explained
+ * Return: Always 0 Success
+ **/
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		char numberChar = number + '0';
+		putchar(numberChar);
+		number++;
+	}
+	putChar("\n");
+	return (0);
+	}

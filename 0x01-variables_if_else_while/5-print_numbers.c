6599b06221e9396b1872802d6bae3725113c8d16
Sameazzy <sammyeazzy@gmail.com>
2022-10-14 21:27:16 -0700

    single numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * entry point explained
+ * Return: Always 0 Success
+ **/
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		printf("%d", number);
+		number++;
+	}
+	printf("\n");
+	return (0);
+	}

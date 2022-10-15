42f6874671c66c38e19f83efd8408e32606163d6
Sameazzy <sammyeazzy@gmail.com>
2022-10-14 21:40:22 -0700

    lowercase alphabet in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * entry point explained
+ * Return: Always 0 Success
+ **/
+int main(void)
+{
+	char alphabet = 'z';
+
+	while (alphabet >= 'a')
+	{
+		putchar(alphabet);
+		alphabet--;
+	}
+	putchar('\n');
+	return (0);
+}

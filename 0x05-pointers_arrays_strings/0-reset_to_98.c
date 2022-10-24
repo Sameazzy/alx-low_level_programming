a32d86599c8753323e20d3a81aa2484d648dd55a
Sameazzy <sammyeazzy@gmail.com>
2022-10-24 14:39:59 -0700

    change the pointer value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - check the code
+ *
+ * update the value 9of a pointer to 98
+ *
+ * @n - the pointer
+ *
+ * The pointer
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

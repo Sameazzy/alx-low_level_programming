561b883fe28e070e250531708dc53cf8d9bc1a44
Sameazzy <sammyeazzy@gmail.com>
2022-10-25 17:13:18 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - check code
+ *
+ * reverse string
+ * @s: string parameter
+ * Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int j;
+	char hold;
+	int length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+
+	for (j = 0; j < length/2; j++)
+	{
+		hold = s[j];
+		s[j] = s[length - j - 1];
+		s[length - j - 1] = hold;
+	}
+}
